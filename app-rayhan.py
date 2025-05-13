bce105645cea80c0cb7dc0d3acaf734b9f73229b
root <root@PC-RAYHAN.>
2025-05-13 09:18:43 +0200

    modif

--- app-rayhan.py
+++ app-rayhan.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, t'es le GOAT"
+    return "Hello,je t'aimes, t'es le GOAT"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
